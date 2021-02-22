b754c1908e0c993379c1bce19574bb0150069419
zhiwenli <lizhiwenzg@gmail.com>
2021-02-22 15:08:23 -0800

    Final fix for timezone issue

--- utils/time.js
+++ utils/time.js
@@ -1,26 +1,41 @@
 /* Return current date in or YYYY-MM-DD format */
 const formattedDate = () => {
-  const time = new Date().toLocaleString(new Date(), {
-    timeZone: 'America/Vancouver',
-  });
-  const year = time.getFullYear();
-  const month = formatWithZero((time.getMonth() + 1).toString());
-  const day = formatWithZero(time.getDate().toString());
+  const { year, month, day } = formatDateTime();
   return `${year}-${month}-${day}`;
 };
 
 /* Return current date in hh:mm:ss format */
 const formattedTime = () => {
-  const time = new Date().toLocaleString(new Date(), {
-    timeZone: 'America/Vancouver',
-  });
-  const hour = formatWithZero(time.getHours().toString());
-  const minute = formatWithZero(time.getMinutes().toString());
-  const second = formatWithZero(time.getSeconds().toString());
+  const { hour, minute, second } = formatDateTime();
   return `${hour}:${minute}:${second}`;
 };
 
+const formatDateTime = () => {
+  const dateTimeString = new Date().toLocaleString(new Date(), {
+    timeZone: 'America/Vancouver',
+  });
+
+  const parts = dateTimeString.split(',');
+  const dateString = parts[0];
+  const timeString = parts[1];
+
+  const dateParts = dateString.split('/');
+  const year = dateParts[2];
+  const month = formatWithZero(dateParts[0]);
+  const day = formatWithZero(dateParts[1]);
+
+  const timeParts = timeString.split(':');
+  const hour = formatWithZero(timeParts[0].slice(1));
+  const minute = formatWithZero(timeParts[1]);
+  const second = formatWithZero(timeParts[2].slice(0, 2));
+
+  return { year, month, day, hour, minute, second };
+};
+
 const formatWithZero = (input) => (input.length < 2 ? `0${input}` : input);
+
+console.log(formattedDate());
+console.log(formattedTime());
 
 module.exports = {
   formattedDate,
