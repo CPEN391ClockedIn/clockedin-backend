ca7edca9af83868f268f15d04d60bb805dc9109d
zhiwenli <lizhiwenzg@gmail.com>
2021-02-21 21:10:20 -0800

    Environment vairables update

--- utils/config.js
+++ utils/config.js
@@ -4,15 +4,6 @@ const { version } = require('../package.json');
 
 const LOG = require('./logger');
 
-LOG.info(`
- ::::::::  :::         ::::::::   ::::::::  :::::::::: ::::::::::: 
-:+:    :+: :+:        :+:    :+: :+:    :+: :+:            :+:     
-+:+        +:+        +:+    +:+ +:+        +:+            +:+     
-+#+        +#+        +#+    +:+ +#++:++#++ +#++:++#       +#+     
-+#+        +#+        +#+    +#+        +#+ +#+            +#+     
-#+#    #+# #+#        #+#    #+# #+#    #+# #+#            #+#     
- ########  ##########  ########   ########  ##########     ###     `);
-
 LOG.info(
   `🚀server startup time: ${new Date().toLocaleString(new Date(), {
     timeZone: 'America/Vancouver',
@@ -23,18 +14,9 @@ LOG.info(`🌲environment:${process.env.NODE_ENV} version:${version}`);
 let PORT = process.env.PORT;
 let MONGODB_URI = process.env.MONGODB_URI;
 
-if (process.env.NODE_ENV === 'test') {
-  PORT = process.env.TEST_PORT;
-  MONGODB_URI = process.env.TEST_MONGODB_URI;
-}
-
 if (process.env.NODE_ENV === 'development') {
   PORT = process.env.DEV_PORT;
   MONGODB_URI = process.env.DEV_MONGODB_URI;
-}
-
-if (process.env.NODE_ENV === 'docker') {
-  MONGODB_URI = process.env.DOCKER_MONGODB_URI;
 }
 
 LOG.info('🔢PORT:', PORT);
