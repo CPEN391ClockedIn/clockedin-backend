f3472c43dcfced639a62616e299848001ceb7b07
zhiwenli <lizhiwenzg@gmail.com>
2021-04-11 23:41:49 -0700

    image 0.8 & clockin 0.1

--- service/loginService.js
+++ service/loginService.js
@@ -43,50 +43,97 @@ const handleAutoLogin = async (employeeId, temperature) => {
     };
   }
 
-  let previousRecord;
-  try {
-    previousRecord = await Temperature.findOneAndUpdate(
-      {
-        date,
-        employee: employeeId,
-      },
-      { temperature },
-      { new: true }
-    );
-  } catch (err) {
-    return {
-      code: 500,
-      message: "Could not not save temperature, please try again later",
-    };
-  }
-
-  if (previousRecord) {
-    return {
-      code: 201,
-      message: "Clocked in successfully and your body temperature has updated",
-    };
-  }
-
-  const newTemperatureRecord = new Temperature({
-    date,
-    temperature,
-    employee: employeeId,
-  });
-
-  try {
-    await newTemperatureRecord.save();
-  } catch (err) {
-    return {
-      code: 500,
-      message: "Could not not save temperature, please try again later",
-    };
-  }
-
   return {
     code: 201,
-    message: "Clocked in successfully and your body temperature has saved",
+    message: "Clocked in successfully",
   };
 };
+
+// const handleAutoLogin = async (employeeId, temperature) => {
+//   const date = formattedDate();
+
+//   /* Check if the employee has clocked in */
+//   let clockInRecord;
+//   try {
+//     clockInRecord = await History.findOne({
+//       date,
+//       employee: employeeId,
+//     });
+//   } catch (err) {
+//     return {
+//       code: 500,
+//       message: "Could not save clock in information, please try again later",
+//     };
+//   }
+
+//   if (clockInRecord) {
+//     return {
+//       code: 403,
+//       message: "Could not clock in twice on the same day!",
+//     };
+//   }
+
+//   const clockInTime = formattedTime();
+
+//   const newClockInRecord = new History({
+//     date,
+//     clockInTime,
+//     employee: employeeId,
+//   });
+
+//   try {
+//     await newClockInRecord.save();
+//   } catch (err) {
+//     return {
+//       code: 500,
+//       message: "Could not save clock in information, please try again later",
+//     };
+//   }
+
+//   let previousRecord;
+//   try {
+//     previousRecord = await Temperature.findOneAndUpdate(
+//       {
+//         date,
+//         employee: employeeId,
+//       },
+//       { temperature },
+//       { new: true }
+//     );
+//   } catch (err) {
+//     return {
+//       code: 500,
+//       message: "Could not not save temperature, please try again later",
+//     };
+//   }
+
+//   if (previousRecord) {
+//     return {
+//       code: 201,
+//       message: "Clocked in successfully and your body temperature has updated",
+//     };
+//   }
+
+//   const newTemperatureRecord = new Temperature({
+//     date,
+//     temperature,
+//     employee: employeeId,
+//   });
+
+//   try {
+//     await newTemperatureRecord.save();
+//   } catch (err) {
+//     return {
+//       code: 500,
+//       message: "Could not not save temperature, please try again later",
+//     };
+//   }
+
+//   return {
+//     code: 201,
+//     message: "Clocked in successfully and your body temperature has saved",
+//   };
+// };
 
 module.exports = {
   handleAutoLogin,
